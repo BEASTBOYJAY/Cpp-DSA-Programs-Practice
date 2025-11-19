9bd780d02ec54e3912a93a3260a73c58497e5284
BEASTBOYJAY <jay31sinha@gmail.com>
2025-11-19 23:14:54 +0530

    added more questions

--- DSA/Arrays_Easy.cpp
+++ DSA/Arrays_Easy.cpp
@@ -387,3 +387,266 @@ using namespace std;
 // }
 
 // Time Complexity - O(N)
+
+// ##################################################################################################
+// ################ PROBLEM: Move all Zeros to the end of the array #################################
+// ##################################################################################################
+
+// ############################ MY APPROACH ###################################
+
+// void movezeros(int arr[], int n)
+// {
+//     int temp[n];
+
+//     int j = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] != 0)
+//         {
+//             temp[j] = arr[i];
+//             j++;
+//         }
+//     }
+
+//     for (int i = j; i < n; i++)
+//     {
+//         temp[i] = 0;
+//     }
+
+//     cout << "new array: " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << temp[i] << " ";
+//     }
+// }
+// Time Complexity: O(N)
+
+// ############################ BRUTE FORCE ###################################
+
+// #include <vector>
+
+// vector<int> moveZeroes(vector<int> &arr)
+// {
+//     // Create temp array
+//     vector<int> temp(arr.size(), 0);
+
+//     // Pointer to fill temp
+//     int index = 0;
+
+//     // Traverse input array
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         // If non-zero, add to temp
+//         if (arr[i] != 0)
+//         {
+//             temp[index] = arr[i];
+//             index++;
+//         }
+//     }
+
+//     // Copy back temp to original
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         arr[i] = temp[i];
+//     }
+
+//     // Return updated array
+//     return arr;
+// }
+// int main()
+// {
+//     vector<int> arr = {0, 1, 0, 3, 12};
+//     vector<int> result = moveZeroes(arr);
+
+//     cout << "Array after moving zeroes: ";
+//     for (int num : result)
+//     {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+// Time Complexity: O(N)
+
+// ############################ OPTIMAL APPROACH ###################################
+
+// #include <vector>
+// #include <algorithm>
+// void moveZeroes(vector<int> &nums)
+// {
+//     int j = -1;
+
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         if (nums[i] == 0)
+//         {
+//             j = i;
+//             break;
+//         }
+//     }
+
+//     if (j == -1)
+//         return;
+
+//     for (int i = j + 1; i < nums.size(); i++)
+//     {
+//         if (nums[i] != 0)
+//         {
+//             swap(nums[i], nums[j]);
+//             j++;
+//         }
+//     }
+// }
+
+// // Time Complexity: O(N)
+// int main()
+// {
+//     vector<int> nums = {0, 1, 0, 3, 12};
+//     moveZeroes(nums);
+
+//     for (int num : nums)
+//         cout << num << " ";
+//     cout << endl;
+//     return 0;
+// }
+
+// #########################################################################
+// ################ PROBLEM: Linear Search #################################
+// #########################################################################
+
+// void linearsearch(int arr[], int tofind, int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == tofind)
+//         {
+//             cout << "Number found in index: " << i;
+//             break;
+//         }
+//     }
+// }
+// Time Complexity: O(n)
+
+// #########################################################################
+// ################ PROBLEM: Union of Two Sorted Arrays ####################
+// #########################################################################
+
+// ############################ 1st APPROACH ###################################
+
+// #include <vector>
+// #include <map>
+
+// vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
+// {
+//     map<int, int> freq;
+//     vector<int> Union;
+//     for (int i = 0; i < n; i++)
+//         freq[arr1[i]]++;
+//     for (int i = 0; i < m; i++)
+//         freq[arr2[i]]++;
+//     for (auto &it : freq)
+//         Union.push_back(it.first);
+//     return Union;
+// }
+
+// // Time Compleixty : O( (m+n)log(m+n) )
+// // Space Complexity : O(m+n) {If Space of Union ArrayList is considered}
+// // O(1) {If Space of union ArrayList is not considered}
+// int main()
+// {
+//     int n = 10, m = 7;
+//     int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+//     vector<int> Union = FindUnion(arr1, arr2, n, m);
+//     cout << "Union of arr1 and arr2 is " << endl;
+//     for (auto &val : Union)
+//         cout << val << " ";
+//     return 0;
+// }
+
+// ############################ 2nd APPROACH ###################################
+
+// #include <vector>
+// #include <set>
+
+// vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
+// {
+//     set<int> s;
+//     vector<int> Union;
+//     for (int i = 0; i < n; i++)
+//         s.insert(arr1[i]);
+//     for (int i = 0; i < m; i++)
+//         s.insert(arr2[i]);
+//     for (auto &it : s)
+//         Union.push_back(it);
+//     return Union;
+// }
+
+// // Time Compleixty : O( (m+n)log(m+n) )
+// // Space Complexity : O(m+n) {If Space of Union ArrayList is considered}
+// // O(1) {If Space of union ArrayList is not considered}
+// int main()
+
+// {
+//     int n = 10, m = 7;
+//     int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+//     vector<int> Union = FindUnion(arr1, arr2, n, m);
+//     cout << "Union of arr1 and arr2 is  " << endl;
+//     for (auto &val : Union)
+//         cout << val << " ";
+//     return 0;
+// }
+
+// ############################ 3rd APPROACH ###################################
+// #include <vector>
+
+// vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
+// {
+//     int i = 0, j = 0;  // pointers
+//     vector<int> Union; // Uninon vector
+//     while (i < n && j < m)
+//     {
+//         if (arr1[i] <= arr2[j]) // Case 1 and 2
+//         {
+//             if (Union.size() == 0 || Union.back() != arr1[i])
+//                 Union.push_back(arr1[i]);
+//             i++;
+//         }
+//         else // case 3
+//         {
+//             if (Union.size() == 0 || Union.back() != arr2[j])
+//                 Union.push_back(arr2[j]);
+//             j++;
+//         }
+//     }
+//     while (i < n) // IF any element left in arr1
+//     {
+//         if (Union.back() != arr1[i])
+//             Union.push_back(arr1[i]);
+//         i++;
+//     }
+//     while (j < m) // If any elements left in arr2
+//     {
+//         if (Union.back() != arr2[j])
+//             Union.push_back(arr2[j]);
+//         j++;
+//     }
+//     return Union;
+// }
+// // Time Complexity: O(m+n)
+// // Space Complexity : O(m+n) {If Space of Union ArrayList is considered}
+// // O(1) {If Space of union ArrayList is not considered}
+
+// int main()
+
+// {
+//     int n = 10, m = 7;
+//     int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+//     vector<int> Union = FindUnion(arr1, arr2, n, m);
+//     cout << "Union of arr1 and arr2 is  " << endl;
+//     for (auto &val : Union)
+//         cout << val << " ";
+//     return 0;
+// }
