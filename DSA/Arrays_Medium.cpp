c6070c06da414e083e54fad3b2868f727e97f503
BEASTBOYJAY <jay31sinha@gmail.com>
2025-11-28 16:58:40 +0530

    added new questions

--- DSA/Arrays_Medium.cpp
+++ DSA/Arrays_Medium.cpp
@@ -903,3 +903,149 @@ using namespace std;
 //     }
 // }
 // Time Complexity: O(N²)
+
+// #########################################################################
+// ################ PROBLEM: Spiral Traversal of Matrix ####################
+// #########################################################################
+
+//  ############################ ONLY APPROACH ###################################
+// vector<int> spiralOrder(vector<vector<int>> &matrix)
+// {
+//     vector<int> result;
+
+//     int top = 0;
+//     int bottom = matrix.size() - 1;
+//     int left = 0;
+//     int right = matrix[0].size() - 1;
+
+//     while (top <= bottom && left <= right)
+//     {
+
+//         for (int i = left; i <= right; i++)
+//         {
+//             result.push_back(matrix[top][i]);
+//         }
+//         top++;
+
+//         for (int i = top; i <= bottom; i++)
+//         {
+//             result.push_back(matrix[i][right]);
+//         }
+//         right--;
+
+//         if (top <= bottom)
+//         {
+//             for (int i = right; i >= left; i--)
+//             {
+//                 result.push_back(matrix[bottom][i]);
+//             }
+//             bottom--;
+//         }
+
+//         if (left <= right)
+//         {
+//             for (int i = bottom; i >= top; i--)
+//             {
+//                 result.push_back(matrix[i][left]);
+//             }
+//             left++;
+//         }
+//     }
+
+//     return result;
+// }
+
+// Time Complexity: O(n x m)
+
+// ##########################################################################
+// ################ PROBLEM: Count Subarray sum Equals K ####################
+// ##########################################################################
+
+// ############################ BRUTE FORCE ###################################
+
+// int subarraySum(vector<int> &arr, int k)
+// {
+//     int n = arr.size();
+
+//     int count = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i; j < n; j++)
+//         {
+//             int sum = 0;
+
+//             for (int m = i; m <= j; m++)
+//             {
+//                 sum += arr[m];
+//             }
+
+//             if (sum == k)
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+
+//     return count;
+// }
+// Time Complexity: O(N3)
+
+// ############################ BETTER APPROACH ###################################
+
+// int subarraySum(vector<int> &arr, int k)
+// {
+//     int n = arr.size();
+
+//     int count = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int sum = 0;
+
+//         for (int j = i; j < n; j++)
+//         {
+//             sum += arr[j];
+
+//             if (sum == k)
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+
+//     return count;
+// }
+// Time Complexity: O(n²)
+
+//  ############################ OPTIMAL APPROACH ###################################
+
+// #include <unordered_map>
+// int subarraySum(vector<int> &arr, int k)
+// {
+//     int n = arr.size();
+
+//     unordered_map<int, int> prefixSumCount;
+
+//     int prefixSum = 0;
+//     int count = 0;
+
+//     prefixSumCount[0] = 1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         prefixSum += arr[i];
+
+//         int remove = prefixSum - k;
+
+//         if (prefixSumCount.find(remove) != prefixSumCount.end())
+//         {
+//             count += prefixSumCount[remove];
+//         }
+
+//         prefixSumCount[prefixSum]++;
+//     }
+
+//     return count;
+// }
+// Time Complexity: O(n)
