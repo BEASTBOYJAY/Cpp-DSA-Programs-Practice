62bdd23cf9b779acf435e7ecfde71db120e315db
BEASTBOYJAY <jay31sinha@gmail.com>
2025-11-30 21:07:53 +0530

    added more questions

--- DSA/Arrays_Hard.cpp
+++ DSA/Arrays_Hard.cpp
@@ -573,3 +573,279 @@ using namespace std;
 // }
 
 // Time Complexity: O(N)
+
+// ##############################################################################
+// ################ PROBLEM: Merge Overlapping Sub-intervals ####################
+// ##############################################################################
+
+// ############################ BRUTE FORCE ###################################
+
+// #include <algorithm>
+// vector<vector<int>> merge(vector<vector<int>> &intervals)
+// {
+
+//     sort(intervals.begin(), intervals.end());
+
+//     vector<vector<int>> ans;
+
+//     int n = intervals.size();
+//     for (int i = 0; i < n;)
+//     {
+
+//         int start = intervals[i][0];
+//         int end = intervals[i][1];
+
+//         int j = i + 1;
+//         while (j < n && intervals[j][0] <= end)
+//         {
+//             end = max(end, intervals[j][1]);
+//             j++;
+//         }
+
+//         ans.push_back({start, end});
+
+//         i = j;
+//     }
+
+//     return ans;
+// }
+
+// Time Complexity: O(N^2)
+
+//  ############################ OPTIMAL APPROACH ###################################
+
+// #include <algorithm>
+// vector<vector<int>> merge(vector<vector<int>> &intervals)
+// {
+//     sort(intervals.begin(), intervals.end());
+
+//     vector<vector<int>> merged;
+
+//     for (auto interval : intervals)
+//     {
+//         if (merged.empty() || merged.back()[1] < interval[0])
+//         {
+//             merged.push_back(interval);
+//         }
+//         else
+//         {
+//             merged.back()[1] = max(
+//                 merged.back()[1],
+//                 interval[1]);
+//         }
+//     }
+
+//     return merged;
+// }
+
+// Time Complexity : O(N *logN) + O(N)
+
+// ##########################################################################################
+// ################ PROBLEM: Merge two Sorted Arrays Without Extra Space ####################
+// ##########################################################################################
+
+//  ############################ ONLY APPROACH ###################################
+
+// void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+// {
+//     int i = m - 1;
+
+//     int j = n - 1;
+
+//     int k = m + n - 1;
+
+//     while (i >= 0 && j >= 0)
+//     {
+//         if (nums1[i] > nums2[j])
+//         {
+//             nums1[k--] = nums1[i--];
+//         }
+//         else
+//         {
+//             nums1[k--] = nums2[j--];
+//         }
+//     }
+
+//     while (j >= 0)
+//     {
+//         nums1[k--] = nums2[j--];
+//     }
+// }
+
+// Time Complexity: O(N+M)
+
+// ##########################################################################################
+// ################ PROBLEM: Find the repeating and missing numbers #########################
+// ##########################################################################################
+
+// ############################ BRUTE FORCE ###################################
+
+// vector<int> findMissingRepeatingNumbers(vector<int> &nums)
+// {
+
+//     int n = nums.size();
+//     int repeating = -1, missing = -1;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+
+//         int cnt = 0;
+
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (nums[j] == i)
+//                 cnt++;
+//         }
+
+//         if (cnt == 2)
+//             repeating = i;
+//         else if (cnt == 0)
+//             missing = i;
+
+//         if (repeating != -1 && missing != -1)
+//             break;
+//     }
+
+//     return {repeating, missing};
+// }
+
+// Time Complexity: O(N2)
+//  ############################ BETTER APPROACH ###################################
+
+// vector<int> findMissingRepeatingNumbers(vector<int> &nums)
+// {
+
+//     int n = nums.size();
+
+//     int hash[n + 1] = {0};
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         hash[nums[i]]++;
+//     }
+
+//     int repeating = -1, missing = -1;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (hash[i] == 2)
+//         {
+//             repeating = i;
+//         }
+//         else if (hash[i] == 0)
+//         {
+//             missing = i;
+//         }
+
+//         if (repeating != -1 && missing != -1)
+//         {
+//             break;
+//         }
+//     }
+
+//     return {repeating, missing};
+// }
+
+// Time Complexity: O(2*N)
+
+// ############################ OPTIMAL APPROACH ###################################
+
+// vector<int> findMissingRepeatingNumbers(vector<int> &nums)
+// {
+
+//     long long n = nums.size();
+
+//     long long SN = (n * (n + 1)) / 2;
+
+//     long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+//     long long S = 0, S2 = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         S += nums[i];
+//         S2 += (long long)nums[i] * (long long)nums[i];
+//     }
+
+//     long long val1 = S - SN;
+
+//     long long val2 = S2 - S2N;
+
+//     val2 = val2 / val1;
+
+//     long long x = (val1 + val2) / 2;
+//     long long y = x - val1;
+
+//     return {(int)x, (int)y};
+// }
+
+// Time Complexity: O(N)
+
+// ############################ OPTIMAL APPROACH ###################################
+
+// vector<int> findMissingRepeatingNumbers(vector<int> &nums)
+// {
+
+//     int n = nums.size();
+
+//     int xr = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         xr = xr ^ nums[i];
+
+//         xr = xr ^ (i + 1);
+//     }
+
+//     int number = (xr & ~(xr - 1));
+
+//     int zero = 0;
+
+//     int one = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         if ((nums[i] & number) != 0)
+//         {
+
+//             one = one ^ nums[i];
+//         }
+//         else
+//         {
+//             zero = zero ^ nums[i];
+//         }
+//     }
+
+//     for (int i = 1; i <= n; i++)
+//     {
+
+//         if ((i & number) != 0)
+//         {
+
+//             one = one ^ i;
+//         }
+//         else
+//         {
+//             zero = zero ^ i;
+//         }
+//     }
+
+//     int cnt = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (nums[i] == zero)
+//         {
+//             cnt++;
+//         }
+//     }
+
+//     if (cnt == 2)
+//     {
+//         return {zero, one};
+//     }
+
+//     return {one, zero};
+// }
+
+// Time Complexity: O(N)
