14219b49bdff61b8a9ab60015a14b243ab1ffe6a
BEASTBOYJAY <jay31sinha@gmail.com>
2025-12-07 22:16:31 +0530

    added more questions

--- DSA/Binary_Search.cpp
+++ DSA/Binary_Search.cpp
@@ -278,3 +278,235 @@ using namespace std;
 // }
 
 // Time Complexity: O(log N)
+
+// ################################################################################
+// ################ PROBLEM: Count Occurrences in Sorted Array ####################
+// ################################################################################
+
+// ############################ BRUTE FORCE ###################################
+
+// int count(vector<int> &arr, int n, int x)
+// {
+//     int cnt = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         if (arr[i] == x)
+//             cnt++;
+//     }
+//     return cnt;
+// }
+
+// Time Complexity: O(N)
+
+// ############################ OPTIMAL APPROACH ###################################
+
+// int firstOccurrence(vector<int> &arr, int n, int k)
+// {
+//     int low = 0, high = n - 1;
+//     int first = -1;
+
+//     while (low <= high)
+//     {
+//         int mid = (low + high) / 2;
+//         if (arr[mid] == k)
+//         {
+//             first = mid;
+//             high = mid - 1;
+//         }
+//         else if (arr[mid] < k)
+//         {
+//             low = mid + 1;
+//         }
+//         else
+//         {
+//             high = mid - 1;
+//         }
+//     }
+//     return first;
+// }
+
+// int lastOccurrence(vector<int> &arr, int n, int k)
+// {
+//     int low = 0, high = n - 1;
+//     int last = -1;
+
+//     while (low <= high)
+//     {
+//         int mid = (low + high) / 2;
+//         if (arr[mid] == k)
+//         {
+//             last = mid;
+//             low = mid + 1;
+//         }
+//         else if (arr[mid] < k)
+//         {
+//             low = mid + 1;
+//         }
+//         else
+//         {
+//             high = mid - 1;
+//         }
+//     }
+//     return last;
+// }
+
+// pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+// {
+//     int first = firstOccurrence(arr, n, k);
+//     if (first == -1)
+//         return {-1, -1};
+//     int last = lastOccurrence(arr, n, k);
+//     return {first, last};
+// }
+
+// int count(vector<int> &arr, int n, int x)
+// {
+//     pair<int, int> ans = firstAndLastPosition(arr, n, x);
+//     if (ans.first == -1)
+//         return 0;
+//     return (ans.second - ans.first + 1);
+// }
+
+// Time Complexity: O(2*logN)
+
+// #######################################################################################
+// ################ PROBLEM: Search Element in a Rotated Sorted Array ####################
+// #######################################################################################
+
+// ############################ BRUTE FORCE ###################################
+
+// int search(vector<int> &nums, int target)
+// {
+
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+
+//         if (nums[i] == target)
+//         {
+//             return i;
+//         }
+//     }
+
+//     return -1;
+// }
+
+// Time Complexity: O(N)
+
+// ############################ OPTIMAL APPROACH ###################################
+
+// int search(vector<int> &nums, int target)
+// {
+
+//     int low = 0;
+//     int high = nums.size() - 1;
+
+//     while (low <= high)
+//     {
+
+//         int mid = (low + high) / 2;
+
+//         if (nums[mid] == target)
+//             return mid;
+
+//         if (nums[low] <= nums[mid])
+//         {
+
+//             if (nums[low] <= target && target < nums[mid])
+//             {
+//                 high = mid - 1;
+//             }
+//             else
+//             {
+//                 low = mid + 1;
+//             }
+//         }
+
+//         else
+//         {
+
+//             if (nums[mid] < target && target <= nums[high])
+//             {
+//                 low = mid + 1;
+//             }
+//             else
+//             {
+//                 high = mid - 1;
+//             }
+//         }
+//     }
+
+//     return -1;
+// }
+
+// Time Complexity: O(log N)
+
+// ########################################################################################
+// ################ PROBLEM: Search Element in Rotated Sorted Array II ####################
+// ########################################################################################
+
+// ############################ BRUTE FORCE ###################################
+
+// bool searchInARotatedSortedArrayII(vector<int> &arr, int k)
+// {
+//     int n = arr.size();
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == k)
+//             return true;
+//     }
+//     return false;
+// }
+
+// Time Complexity: O(N)
+
+// ############################ OPTIMAL APPROACH ###################################
+
+// bool searchInARotatedSortedArrayII(vector<int> &arr, int k)
+// {
+//     int n = arr.size();
+//     int low = 0, high = n - 1;
+
+//     while (low <= high)
+//     {
+//         int mid = (low + high) / 2;
+
+//         if (arr[mid] == k)
+//             return true;
+
+//         if (arr[low] == arr[mid] && arr[mid] == arr[high])
+//         {
+//             low++;
+//             high--;
+//             continue;
+//         }
+
+//         if (arr[low] <= arr[mid])
+//         {
+//             if (arr[low] <= k && k <= arr[mid])
+//             {
+//                 high = mid - 1;
+//             }
+//             else
+//             {
+//                 low = mid + 1;
+//             }
+//         }
+//         else
+//         {
+
+//             if (arr[mid] <= k && k <= arr[high])
+//             {
+//                 low = mid + 1;
+//             }
+//             else
+//             {
+//                 high = mid - 1;
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
+// Time Complexity: O(logN)
