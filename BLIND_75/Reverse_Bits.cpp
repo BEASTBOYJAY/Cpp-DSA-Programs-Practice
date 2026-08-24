15bd43df893a15cb30ce401a689e7438767d14f9
BEASTBOYJAY <jay31sinha@gmail.com>
2026-08-24 17:03:24 +0530

    Reverse_Bits

---
+++ BLIND_75/Reverse_Bits.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverseBits(int n) {
+    int res = 0;
+    for (int i = 0; i < 32; i++) {
+        int bit = (n >> i) & 1;
+        res = res | (bit << (31 - i));
+    }
+    return res;
+}
+
+int main()
+{
+    cout<<reverseBits(43261596);
+}
